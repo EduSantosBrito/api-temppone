d64b88000861fafbc7994b1b634d1ccba34bf075
EduSantosBrito <edu.santos.brito@gmail.com>
2021-07-16 17:00:44 -0300

    fix: fastify host

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ fastify.register(require("./routes/auth"), { prefix: "/auth" });
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(process.env.PORT || 3000);
+    await fastify.listen(process.env.PORT || 3000, "0.0.0.0");
     await Database.connect();
     console.log("MongoDB Connected successfully :)");
   } catch (err) {
